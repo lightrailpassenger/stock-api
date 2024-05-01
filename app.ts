c615af5dfb6dcf33eab55705cbcfa6659062bcec
Tony Chow <tonychow.0929@icloud.com>
2024-05-01 19:45:08 +0800

    Basic support of dividend info

--- app.ts
+++ app.ts
@@ -1,6 +1,10 @@
 import { Application, Router } from "oak";
 
 import { fetchHtmlFromYahooFinance, parseInfo } from "./query-yahoo.ts";
+import {
+  fetchDividendHtmlFromAASTOCKS,
+  parseDividend,
+} from "./query-aastocks.ts";
 import { rectifyTickerSymbol } from "./ticker-util.ts";
 
 const router = new Router();
@@ -44,6 +48,41 @@ router.get("/price-info", async (context) => {
     context.response.body = { ts: actualTs, ...info };
     context.response.status = 200;
   } catch (err) {
+    console.error(err);
+
+    context.response.body = { err: "INTERNAL_SERVER_ERROR" };
+    context.response.status = 500;
+  }
+});
+
+router.get("/dividend", async (context) => {
+  try {
+    const query = context.request.url.searchParams;
+    const urlSearchParams = new URLSearchParams(query);
+    const ts = urlSearchParams.get("ts");
+
+    if (!ts) {
+      context.response.body = { err: "BAD_REQUEST" };
+      context.response.status = 400;
+
+      return;
+    }
+
+    const html = await fetchDividendHtmlFromAASTOCKS(ts);
+    const entries = parseDividend(html);
+
+    context.response.body = { div: entries };
+    context.response.status = 200;
+  } catch (err) {
+    const { status } = err;
+
+    if (status === 404) {
+      context.response.body = { err: "NOT_FOUND" };
+      context.response.status = 404;
+
+      return;
+    }
+
     console.error(err);
 
     context.response.body = { err: "INTERNAL_SERVER_ERROR" };
---
+++ query-aastocks.ts
@@ -0,0 +1,83 @@
+import * as regexp from "regexp";
+
+const getUrlFromTickerSymbol = (ts: string): string => {
+  return `http://www.aastocks.com/tc/stocks/analysis/dividend.aspx?symbol=${
+    encodeURIComponent(ts)
+  }`;
+};
+
+const getHKTickerSymbol = (ts: string): string | null => {
+  const numTs = parseInt(ts, 10);
+
+  if (!numTs) {
+    return null;
+  }
+
+  return `${numTs}`.padStart(5, "0");
+};
+
+const fetchDividendHtmlFromAASTOCKS = async (ts: string): string => {
+  const numTs = getHKTickerSymbol(ts);
+
+  if (!numTs) {
+    throw Object.assign(new Error("Cannot parse ticker symbol"), {
+      status: 404,
+    });
+  }
+
+  const url = getUrlFromTickerSymbol(numTs);
+  const result = await globalThis.fetch(url, { method: "GET" });
+
+  if (!result.ok) {
+    throw Object.assign(new Error("Cannot fetch from AASTOCKS"), {
+      status: result.status,
+    });
+  }
+
+  return await result.text();
+};
+
+const currencySymbolToFirstCharacterRegex = {
+  "HKD": "港",
+  "USD": "美",
+  "EUR": "歐",
+  "JPY": "日",
+  "CNH": "人",
+} as const;
+const dividendRegex = new RegExp(
+  `\\s*<td class="[^"]+">(?<year>20[0-9][0-9])/(?<date>[0-9]{2}/[0-9]{2})[^${
+    regexp.escape("普")
+  }]*${regexp.escape("普通股息")}</a>[\x00-\x7f]*${
+    regexp.escape("：")
+  }?(?<currency>[${
+    Object.values(currencySymbolToFirstCharacterRegex).map((
+      c,
+    ) => (regexp.escape(c)))
+  }])[^0-9]*(?<amount>[0-9.]+)`,
+  "g",
+);
+
+type DividendEntry = {
+  year: string;
+  date: string;
+  currency: string;
+  amount: number;
+};
+
+const parseDividend = (html: string): DividendEntry[] => {
+  const matched = html.split("<tr>").flatMap((
+    item,
+  ) => [...item.matchAll(dividendRegex)]);
+
+  return matched.filter(({ groups }) => groups)
+    .map(({ groups }) => ({
+      year: groups.year,
+      date: groups.date,
+      amount: Number(groups.amount),
+      currency: Object.entries(currencySymbolToFirstCharacterRegex).find((
+        [, c],
+      ) => (c === groups.currency))[0] ?? "Unknown",
+    }));
+};
+
+export { fetchDividendHtmlFromAASTOCKS, parseDividend };
